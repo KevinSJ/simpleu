624de28c963947894910c08eb5eba7d84fb51dad
vokins <vokins@gmail.com>
2015-01-31 15:18:06 +0800

    clrs

--- data/@data.bat
+++ data/@data.bat
@@ -1,4 +1,4 @@
-del /f 360.txt Mwsl.txt 1.txt 2.txt grd.txt
+del /f 360.txt Mwsl.txt 1.txt 2.txt
 wget -q -O- http://webscan.360.cn/url | gawk "BEGIN {print \"#360 MTPL\"};/\.html/{print $0=gensub(/.*url\/(.+)\.html.*/,\"127.0.0.1 \\1\",\"1\")}">1.txt
 gawk "!a[$0]++" 1.txt >360.txt
 rem wget -q -O- http://www.mwsl.org.cn/hosts/hosts | gawk "NR>10{print \"127.0.0.1\",$2}">2.txt
@@ -8,13 +8,13 @@ set p1=http://serve.netsh.org/pub/ping.php
 set p2=http://tools.pingdom.com/ping/
 set p3=http://ping.eu/ping
 set p4=http://www.360kb.com/kb/2_122.html
-rem http://bbs.a9vg.com/thread-3476870-1-1.htmlhttp://www.right.com.cn/forum/thread-152514-1-1.html http://www.abclite.org/67 http://heartnn.gitcafe.io/hosts/
+rem http://bbs.a9vg.com/thread-3476870-1-1.html http://www.right.com.cn/forum/thread-152514-1-1.html http://www.abclite.org/67
 set s1=http://blog.crpuer.com/GavinHosts.txt
 set s2=https://raw.githubusercontent.com/txthinking/google-hosts/master/hosts
 set s3=http://git.oschina.net/jiange1236/googlehosts/tree/master
 set s4=https://www.projecth.us/sources
 @echo off
-ver=0.2.3.6
+ver=0.2.3.7
 SetLocal EnableExtensions
 SetLocal EnableDelayedExpansion
 set str=%date:~0,4%%date:~5,2%00
@@ -52,7 +52,7 @@ echo del %%0 >>"%~dp0..\tools\7z.bat"
 goto :eof
 
 :SDall
-set files=bat.txt Version.txt redirect.txt grd.txt 1listrds.txt mobile.txt msoft.txt xunlei.txt game.txt active.txt soft.txt site.txt sitecn.txt sitecbs.txt down.txt 360.txt porn.txt email.txt operators.txt popups.txt
+set files=bat.txt Version.txt redirect.txt 1listrds.txt mobile.txt msoft.txt xunlei.txt game.txt active.txt soft.txt site.txt sitecn.txt sitecbs.txt down.txt 360.txt porn.txt email.txt operators.txt popups.txt
 for %%a in (%files%) do (type "%%a">>1A.txt)
 goto :eof
 
@@ -147,14 +147,11 @@ sed -i "/5isotoi5.org/d" hosts
 sed -i "/.sandai.net/d" hosts
 sed -i "/flashget.com/d" hosts
 sed -i "s/\t/ /g" hosts
-rem ½«ÎÄ¼þÄÚµÄTABÌæ»»Îª¿Õ¸ñ
 goto :eof
 
 wget -c --no-check-certificate -O grd.txt %s3%
 sed -i "s/\t/ /g" grd.txt
-rem ½«ÎÄ¼þÄÚµÄTABÌæ»»Îª¿Õ¸ñ
 sed -i "s/[ ]\{2,\}/ /g" grd.txt
-rem ½«ÎÄ¼þÄÚµÄÈý¸ö¿Õ¸ñÌæ»»Îª¿Õ¸ñ
 sed -i "1,55d" grd.txt
 sed -i "/googlesyndication/d" grd.txt
 sed -i "/google-analytics/d" grd.txt
@@ -168,15 +165,9 @@ sed -i "1i\#redirect" grd.txt
 gawk "!a[$0]++" grd.txt >rd3rd.txt
 del /f grd.txt 2.txt
 
-
----
-wget -c --no-check-certificate -O gg.txt %s3%
 sed -i "s/\t/ /g" gg.txt
-rem ½«ÎÄ¼þÄÚµÄTABÌæ»»Îª¿Õ¸ñ
 sed -i "s/[ ]\{2,\}/ /g" gg.txt
-rem ½«ÎÄ¼þÄÚµÄÈý¸ö¿Õ¸ñÌæ»»Îª¿Õ¸ñ
 sed -i "/#Google Services END/q" gg.txt
-rem É¾³ýgoogleÏÂÃæµÄÐÐÄÚÈÝ
 sed -i "/googlesyndication/d" gg.txt
 sed -i "/google-analytics/d" gg.txt
 sed -i "/googleadservices/d" gg.txt
